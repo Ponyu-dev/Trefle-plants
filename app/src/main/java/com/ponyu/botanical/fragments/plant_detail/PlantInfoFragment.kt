8c6f906204b415a9368f1b8464fd7bc660759bad
Ponyu-dev <mopoypa@yandex.ru>
2024-03-10 09:22:29 +0300

    Made a display of basic information about the plant.

--- app/src/main/java/com/ponyu/botanical/fragments/plant_detail/PlantInfoFragment.kt
+++ app/src/main/java/com/ponyu/botanical/fragments/plant_detail/PlantInfoFragment.kt
@@ -1,15 +1,27 @@
 package com.ponyu.botanical.fragments.plant_detail
 
+import android.graphics.Paint.Style
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.EmojiConsistency
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.ponyu.botanical.R
+import com.ponyu.botanical.data.remote.plant.MainSpecies
 import com.ponyu.botanical.databinding.FragmentPlantInfoBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -38,21 +50,39 @@ class PlantInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        withBinding {
-            viewLifecycleOwner.lifecycleScope.launch {
-                repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    plantInfoViewModel.plantInfo.collect {
-                        it?.data.let {plant ->
-                            Log.d(TAG, "commonName ${plant?.mainSpecies?.commonName}")
-
-                            textView.text = plant?.mainSpecies?.commonName
-                        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                plantInfoViewModel.plantInfo.collect {
+                    it?.data?.mainSpecies?.let {
+                        updateMainInfo(it)
                     }
                 }
             }
         }
 
         plantInfoViewModel.loadPlantInfo(plantId ?: PLANT_ID_COCONUT)
+    }
+
+    private fun updateMainInfo(
+        mainSpecies: MainSpecies
+    ){
+        withBinding {
+            textViewPlantScientificName.text = mainSpecies.scientificName
+            textViewPlantCommonName.text = mainSpecies.commonName
+
+            val spannable1 = SpannableString("\uD83D\uDCD7 ${mainSpecies.author} ${mainSpecies.year} - ")
+            val spannable2 = SpannableString(mainSpecies.bibliography)
+            spannable2.setSpan(StyleSpan(Typeface.ITALIC), 0, spannable2.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            textViewPlantFirstPublish.text = SpannableStringBuilder(spannable1).append(spannable2)
+
+            Glide.with(this@PlantInfoFragment)
+                .load(mainSpecies.imageUrl)
+                .placeholder(R.drawable.image_placeholder)
+                .error(R.drawable.image_not_found)
+                .centerCrop()
+                .into(imageViewPlant)
+        }
     }
 
     override fun onDestroyView() {
